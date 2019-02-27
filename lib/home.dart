dc07e0d57840ae4e273cddc84676228b283f13ab
Adriano Massi <adrianomassi@gmail.com>
2019-02-28 00:36:04 +0100

    Set up drawer

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import "package:flutter/material.dart";
+
+class Home extends StatelessWidget {
+
+  Widget _buildSideDrawer(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            automaticallyImplyLeading: false,
+            title: Text('Choose'),
+          ),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage Products'),
+            onTap: () {
+              
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: _buildSideDrawer(context),
+      appBar: AppBar(
+        title: Text('WCCM'),
+       
+      ),
+      body: Text("Ciao"),
+    );
+  }
+}
