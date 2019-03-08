d2cbee3dda51ede25e1e7885342009b96334b163
Adriano Massi <adrianomassi@gmail.com>
2019-03-08 01:43:48 +0100

    news list working (not ui yet)

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
     var itemModel = NewsModel.fromJson(json.decode(response.body));
     
     setState(() {
-      allTheNews = itemModel;
+      allTheNews.add(itemModel);
     });    
   }
 
