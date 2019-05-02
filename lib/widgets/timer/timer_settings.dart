bfb232b2aaced14f3177f74c9715ea231496d9d7
Adriano Massi <adrianomassi@gmail.com>
2019-05-02 08:35:42 +0200

    Timer Settings Layout

--- lib/widgets/timer/timer_settings.dart
+++ lib/widgets/timer/timer_settings.dart
@@ -11,19 +11,19 @@ class TimerSettings extends StatefulWidget {
 
 class _TimerSettingsState extends State<TimerSettings> {
   Duration _duration = Duration(hours: 0, minutes: 0);
+  int minutes = 20;
 
   Timer _timer;
 
   void startTimer() {
     const oneSec = const Duration(seconds: 1);
-    int _seconds = _duration.inSeconds;
     _timer = Timer.periodic(
         oneSec,
         (Timer timer) => setState(() {
-              if (_seconds < 1) {
+              if (minutes < 1) {
                 timer.cancel();
               } else {
-                _seconds = _seconds - 1;
+                minutes = minutes - 1;
               }
             }));
   }
@@ -41,24 +41,7 @@ class _TimerSettingsState extends State<TimerSettings> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Expanded(
-              // Use it from the context of a stateful widget, passing in
-              // and saving the duration as a state variable.
-              child: DurationPicker(
-            duration: _duration,
-            onChange: (val) {
-              this.setState(() => _duration = val);
-            },
-            //snapToMins: 5.0,
-          )),
-          RaisedButton(
-            onPressed: () {
-              startTimer();
-            },
-            child: Text("start"),
-          ),
-          Text("$_duration"),
-          RaisedButton(
+          FlatButton(
             onPressed: () async {
               // Use it as a dialog, passing in an optional initial time
               // and returning a promise that resolves to the duration
@@ -67,11 +50,26 @@ class _TimerSettingsState extends State<TimerSettings> {
                 context: context,
                 initialTime: new Duration(minutes: 20),
               );
-              this.setState(() => _duration = resultingDuration);
+              this.setState(
+                () {
+                  _duration = resultingDuration;
+                  minutes = _duration.inSeconds;
+                },
+              );
               Scaffold.of(context).showSnackBar(new SnackBar(
                   content: new Text("Chose duration: $resultingDuration")));
             },
-            child: Text("Popup"),
+            child: Text(
+              '$minutes',
+              style: TextStyle(
+                  fontSize: 20, fontWeight: FontWeight.w900, color: Colors.red),
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              startTimer();
+            },
+            child: Text("start"),
           ),
         ],
       ),
