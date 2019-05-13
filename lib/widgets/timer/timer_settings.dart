bc5e5dc98a0920f415afc66543b4a91e2a819ba9
Adriano Massi <adrianomassi@gmail.com>
2019-05-13 16:01:38 +0200

    Timer settings

--- lib/widgets/timer/timer_settings.dart
+++ lib/widgets/timer/timer_settings.dart
@@ -11,7 +11,9 @@ class TimerSettings extends StatefulWidget {
 
 class _TimerSettingsState extends State<TimerSettings> {
   Duration _duration = Duration(hours: 0, minutes: 0);
-  int minutes = 20;
+  int prep_minutes = 20;
+  int med_minutes = 1;
+  String prep_label = '';
 
   Timer _timer;
 
@@ -20,10 +22,10 @@ class _TimerSettingsState extends State<TimerSettings> {
     _timer = Timer.periodic(
         oneSec,
         (Timer timer) => setState(() {
-              if (minutes < 1) {
+              if (prep_minutes < 1) {
                 timer.cancel();
               } else {
-                minutes = minutes - 1;
+                prep_minutes = prep_minutes - 1;
               }
             }));
   }
@@ -46,21 +48,46 @@ class _TimerSettingsState extends State<TimerSettings> {
               // Use it as a dialog, passing in an optional initial time
               // and returning a promise that resolves to the duration
               // chosen when the dialog is accepted. Null when cancelled.
-              Duration resultingDuration = await showDurationPicker(
+              Duration preparationDuration = await showDurationPicker(
                 context: context,
                 initialTime: new Duration(minutes: 20),
               );
               this.setState(
                 () {
-                  _duration = resultingDuration;
-                  minutes = _duration.inSeconds;
+                  prep_label = '{$_duration.inHours}:{$_duration.inMinutes}';
+                  _duration = preparationDuration;
+                  prep_minutes = _duration.inSeconds;
                 },
               );
               Scaffold.of(context).showSnackBar(new SnackBar(
-                  content: new Text("Chose duration: $resultingDuration")));
+                  content: new Text("Chose dura  tion: $preparationDuration")));
             },
             child: Text(
-              '$minutes',
+              'Preparation: $prep_label',
+              style: TextStyle(
+                  fontSize: 20, fontWeight: FontWeight.w900, color: Colors.red),
+            ),
+          ),
+          FlatButton(
+            onPressed: () async {
+              // Use it as a dialog, passing in an optional initial time
+              // and returning a promise that resolves to the duration
+              // chosen when the dialog is accepted. Null when cancelled.
+              Duration meditationDuration = await showDurationPicker(
+                context: context,
+                initialTime: new Duration(minutes: 20),
+              );
+              this.setState(
+                () {
+                  _duration = meditationDuration;
+                  med_minutes = _duration.inSeconds;
+                },
+              );
+              Scaffold.of(context).showSnackBar(new SnackBar(
+                  content: new Text("Chose duration: $meditationDuration")));
+            },
+            child: Text(
+              'Meditation time: $med_minutes',
               style: TextStyle(
                   fontSize: 20, fontWeight: FontWeight.w900, color: Colors.red),
             ),
@@ -69,7 +96,7 @@ class _TimerSettingsState extends State<TimerSettings> {
             onPressed: () {
               startTimer();
             },
-            child: Text("start"),
+            child: Text("Meditate"),
           ),
         ],
       ),
