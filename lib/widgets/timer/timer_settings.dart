056a9e5f220237b0506ece9ebb1ae0c4bdf6cea9
Adriano Massi <adrianomassi@gmail.com>
2019-05-14 16:26:01 +0200

    Timer settings layout, added bowl

--- lib/widgets/timer/timer_settings.dart
+++ lib/widgets/timer/timer_settings.dart
@@ -10,10 +10,12 @@ class TimerSettings extends StatefulWidget {
 }
 
 class _TimerSettingsState extends State<TimerSettings> {
-  Duration _duration = Duration(hours: 0, minutes: 0);
+  Duration _prep_duration = Duration(hours: 0, minutes: 0);
+  Duration _med_duration = Duration(hours: 0, minutes: 0);
   int prep_minutes = 20;
-  int med_minutes = 1;
-  String prep_label = '';
+  int prep_hours = 0;
+  int med_hours = 0;
+  int med_minutes = 0;
 
   Timer _timer;
 
@@ -40,59 +42,71 @@ class _TimerSettingsState extends State<TimerSettings> {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          FlatButton(
-            onPressed: () async {
-              // Use it as a dialog, passing in an optional initial time
-              // and returning a promise that resolves to the duration
-              // chosen when the dialog is accepted. Null when cancelled.
-              Duration preparationDuration = await showDurationPicker(
-                context: context,
-                initialTime: new Duration(minutes: 20),
-              );
-              this.setState(
-                () {
-                  prep_label = '{$_duration.inHours}:{$_duration.inMinutes}';
-                  _duration = preparationDuration;
-                  prep_minutes = _duration.inSeconds;
-                },
-              );
-              Scaffold.of(context).showSnackBar(new SnackBar(
-                  content: new Text("Chose dura  tion: $preparationDuration")));
-            },
-            child: Text(
-              'Preparation: $prep_label',
-              style: TextStyle(
-                  fontSize: 20, fontWeight: FontWeight.w900, color: Colors.red),
-            ),
-          ),
-          FlatButton(
-            onPressed: () async {
-              // Use it as a dialog, passing in an optional initial time
-              // and returning a promise that resolves to the duration
-              // chosen when the dialog is accepted. Null when cancelled.
-              Duration meditationDuration = await showDurationPicker(
-                context: context,
-                initialTime: new Duration(minutes: 20),
-              );
-              this.setState(
-                () {
-                  _duration = meditationDuration;
-                  med_minutes = _duration.inSeconds;
-                },
-              );
-              Scaffold.of(context).showSnackBar(new SnackBar(
-                  content: new Text("Chose duration: $meditationDuration")));
-            },
-            child: Text(
-              'Meditation time: $med_minutes',
-              style: TextStyle(
-                  fontSize: 20, fontWeight: FontWeight.w900, color: Colors.red),
+          Image.asset('assets/images/bowl.png',
+              width: MediaQuery.of(context).size.width * 0.3),
+          Container(
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () async {
+                    // Use it as a dialog, passing in an optional initial time
+                    // and returning a promise that resolves to the duration
+                    // chosen when the dialog is accepted. Null when cancelled.
+                    Duration preparationDuration = await showDurationPicker(
+                      context: context,
+                      initialTime: _prep_duration,
+                    );
+                    this.setState(
+                      () {
+                        _prep_duration = preparationDuration;
+                        prep_hours = _prep_duration.inHours;
+                        prep_minutes = _prep_duration.inMinutes;
+                      },
+                    );
+                    //Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("Chose duration: $preparationDuration")));
+                  },
+                  child: Text(
+                    'Preparation: ${_prep_duration.inHours}:${_prep_duration.inMinutes}',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.red),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () async {
+                    // Use it as a dialog, passing in an optional initial time
+                    // and returning a promise that resolves to the duration
+                    // chosen when the dialog is accepted. Null when cancelled.
+                    Duration meditationDuration = await showDurationPicker(
+                      context: context,
+                      initialTime: _med_duration,
+                    );
+                    this.setState(
+                      () {
+                        _med_duration = meditationDuration;
+                        med_hours = _med_duration.inHours;
+                        med_minutes = _med_duration.inMinutes;
+                      },
+                    );
+                    //Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("Chose duration: $meditationDuration")));
+                  },
+                  child: Text(
+                    'Meditation: ${_med_duration.inHours}:${_med_duration.inMinutes}',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.red),
+                  ),
+                ),
+              ],
             ),
           ),
           RaisedButton(
+            color: Colors.blueGrey,
             onPressed: () {
               startTimer();
             },
