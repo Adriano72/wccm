74bb35350750a0e5718e7737054ffe4cc5cf978f
Adriano Massi <adrianomassi@gmail.com>
2019-07-22 00:50:50 +0200

    Timer funzionante, screen lock and shared preferences

--- lib/widgets/timer/timer_settings.dart
+++ lib/widgets/timer/timer_settings.dart
@@ -4,6 +4,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:toast/toast.dart';
+import 'package:wakelock/wakelock.dart';
 
 class TimerSettings extends StatefulWidget {
   @override
@@ -21,6 +23,11 @@ class _TimerSettingsState extends State<TimerSettings> {
   bool timerStarted = false;
   Timer timer;
 
+  void showToast(String message) {
+    Toast.show(message, context,
+        duration: Toast.LENGTH_LONG, gravity: Toast.CENTER);
+  }
+
   static AudioPlayer advancedPlayer = AudioPlayer();
   AudioCache player = AudioCache(
       respectSilence: false, prefix: 'sounds/', fixedPlayer: advancedPlayer);
@@ -28,25 +35,34 @@ class _TimerSettingsState extends State<TimerSettings> {
   @override
   void initState() {
     player.load('LaurenceBowlEnd.mp3');
+    _getStoredMedTime();
     super.initState();
   }
 
   void startTimer() {
+    Wakelock.toggle(on: true);
     const oneSec = const Duration(seconds: 1);
-    timer = Timer.periodic(
-      oneSec,
-      (timer) => setState(
-            () {
-              if (_med_duration.inSeconds < 1) {
-                timer.cancel();
-                timerStarted = false;
-              } else {
-                this.setState(() => _med_duration = _med_duration - oneSec);
-                print("Time left: $_med_duration");
-              }
-            },
-          ),
-    );
+    Future.delayed(const Duration(seconds: 10), () {
+      player.play('LaurenceBowlEnd.mp3');
+      timer = Timer.periodic(
+        oneSec,
+        (timer) => setState(
+          () {
+            if (_med_duration.inSeconds < 1) {
+              player.play('LaurenceBowlEnd.mp3');
+              timer.cancel();
+              timerStarted = false;
+              showToast('Session ended');
+              _getStoredMedTime();
+              Wakelock.toggle(on: false);
+            } else {
+              this.setState(() => _med_duration = _med_duration - oneSec);
+              print("Time left: $_med_duration");
+            }
+          },
+        ),
+      );
+    });
   }
 
   @override
@@ -57,16 +73,31 @@ class _TimerSettingsState extends State<TimerSettings> {
 
   String formatTime(Duration rawDuration) {
     if (rawDuration.inHours > 0) {
-      return '${_med_duration.inHours}:${_med_duration.inMinutes}:${_med_duration.inSeconds.remainder(60)}';
+      return '${_med_duration.inHours}:${_med_duration.inMinutes.remainder(60)}:${_med_duration.inSeconds.remainder(60)}';
     } else {
       return '${_med_duration.inMinutes}:${_med_duration.inSeconds.remainder(60)}';
     }
   }
 
-  _storePrepTime(_prepTime) async {
+  void _storeMedTime(int hours, int minutes) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    int preparationTime = _prepTime;
-    await prefs.setInt('counter', preparationTime);
+    await prefs.setInt('timerPresetHours', hours);
+    await prefs.setInt('timerPresetsMinutes', minutes.remainder(60));
+  }
+
+  Future<Map> _getStoredMedTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Map<String, int> storedTime = {};
+    int storedMedTimeHour = (prefs.getInt('timerPresetHours'));
+    int storedMedTimeMinutes = (prefs.getInt('timerPresetsMinutes'));
+    storedTime = {'hours': storedMedTimeHour, 'minutes': storedMedTimeMinutes};
+    setState(() {
+      _med_duration =
+          Duration(hours: storedMedTimeHour, minutes: storedMedTimeMinutes);
+    });
+    print(
+        'STORED MED TIME IS ${storedTime['hours']} HOURS AND ${storedTime['minutes']} MINUTES.');
+    return storedTime;
   }
 
   @override
@@ -90,16 +121,20 @@ class _TimerSettingsState extends State<TimerSettings> {
                       context: context,
                       initialTime: _med_duration,
                     );
-                    this.setState(
-                      () {
-                        _med_duration = meditationDuration;
-                        med_hours = _med_duration.inHours;
-                        med_minutes = _med_duration.inMinutes;
+                    if (meditationDuration != null) {
+                      this.setState(
+                        () {
+                          _med_duration = meditationDuration;
+                          med_hours = _med_duration.inHours;
+                          med_minutes = _med_duration.inMinutes.remainder(60);
 
-                        print("Med hours: $med_hours");
-                        print("Med minutes: $med_minutes");
-                      },
-                    );
+                          print("Med hours: $med_hours");
+                          print("Med minutes: $med_minutes");
+                        },
+                      );
+                    }
+                    _storeMedTime(meditationDuration.inHours,
+                        meditationDuration.inMinutes);
                     //Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("Chose duration: $meditationDuration")));
                   },
                   child: Text(
@@ -118,21 +153,24 @@ class _TimerSettingsState extends State<TimerSettings> {
               ],
             ),
           ),
+          //:TODO Gestire che il timer parta se il tempo è maggiore di zero
           RaisedButton(
             color: Colors.deepOrange,
             onPressed: () {
               if (timerStarted) {
+                Wakelock.toggle(on: false);
                 timer.cancel();
                 advancedPlayer.stop();
                 this.setState(
                   () => timerStarted = false,
                 );
+                _getStoredMedTime();
               } else {
-                player.play('LaurenceBowlEnd.mp3');
                 this.setState(
                   () => timerStarted = true,
                 );
                 print("BUTTON PRESSED");
+                showToast('Session will start in 10 seconds...');
                 startTimer();
               }
             },
